83a49aabdb3019c94b630cc2179cf789c1e7f60b
김예안 <kjan0929@naver.com>
2025-10-27 02:36:22 +0900

    test(validate): 입력값 성공 케이스 테스트 코드 추가 및 에러 케이스 항목 추가
    - '정상적인 입력값, 쉼표 사이 공백이 오는 경우'에 대한 입력값 검증 테스트 코드 추가
    - '쉼표가 연속 2개 이상 오는 경우'에 대한 오류 케이스 항목 추가

--- __tests__/InputValidatorTest.js
+++ __tests__/InputValidatorTest.js
@@ -1,11 +1,12 @@
 import { MESSAGES } from '../src/constants';
 import InputValidator from '../src/Validator';
 
-describe('입력값 검증 테스트', () => {
+describe('입력값 검증 테스트: (에러 케이스)', () => {
   test.each([
     ['특수 문자(;)', 'pobi;woni', '3', MESSAGES.INVALID_SEPERATOR],
     ['공백 이름', '', '3', MESSAGES.INVALID_CHAR_LIMIT],
     ['쉼표로 끝남', 'pobi,', '3', MESSAGES.INVALID_CHAR_LIMIT],
+    ['쉼표 연속 2개 이상', 'pobi,,woni,,,', '3', MESSAGES.INVALID_CHAR_LIMIT],
     ['6자 이상', 'pobi,javaji', '3', MESSAGES.INVALID_CHAR_LIMIT],
     ['시도 횟수(문자)', 'pobi,woni', 'abc', MESSAGES.INVALID_ATTEMPTS_TYPE],
     [
@@ -18,5 +19,15 @@ describe('입력값 검증 테스트', () => {
   ])('입력값 검증 테스트: %s', (_, cars, attempts, expectedError) => {
     const validator = new InputValidator(cars, attempts);
     expect(() => validator.run()).toThrow(expectedError);
+  });
+});
+
+describe('입력값 검증 테스트: (성공 케이스)', () => {
+  test.each([
+    ['정상적인 입력', 'pobi,woni,jun', '5'],
+    ['쉼표 사이 공백', 'pobi, woni,  jun', '3'],
+  ])('입력값 검증 테스트: %s', (_, cars, attempts) => {
+    const validator = new InputValidator(cars, attempts);
+    expect(() => validator.run()).not.toThrow();
   });
 });
