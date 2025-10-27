2815b2e431dd4c8d92b41ac6111904188be9147f
김예안 <kjan0929@naver.com>
2025-10-27 15:52:15 +0900

    feat(app): 앱 전체 실행 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Console } from '@woowacourse/mission-utils';
+import getInputValues from './input.js';
+import parseIntoArray from './parse.js';
+import play from './play.js';
+import Car from './Car.js';
+import announceTheWinner from './pick.js';
+import { MESSAGES } from './constants.js';
+
 class App {
-  async run() {}
+  async run() {
+    const { cars, attempts } = await getInputValues();
+    const parsedCars = parseIntoArray(cars);
+    const attemptsCount = Number(attempts);
+
+    const results = [];
+    const carList = new Map();
+
+    parsedCars.forEach((car) => carList.set(car, new Car(car, '')));
+
+    Array.from({ length: attemptsCount }).forEach(() => {
+      play(carList, results);
+    });
+
+    Console.print(MESSAGES.GAME_RESULT);
+    Console.print(results.join('\n'));
+
+    announceTheWinner(carList);
+  }
 }
 
 export default App;
