8b3844451b17a57ff50aa9294a128202b1a3acd9
김예안 <kjan0929@naver.com>
2025-10-27 15:50:57 +0900

    docs(car): Car 클래스 생성자 JSDoc param 주석 추가

--- src/Car.js
+++ src/Car.js
@@ -1,6 +1,11 @@
 import { Random } from '@woowacourse/mission-utils';
 
 export default class Car {
+  /**
+   *
+   * @param {string} name - 자동차 이름
+   * @param {string} position - 자동차 위치
+   */
   constructor(name, position) {
     this.name = name;
     this.position = position;
